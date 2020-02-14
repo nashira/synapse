c8cb5bf22e36e4dd0ff5c1e5d5344e9305409bcd
Nash <nash@nashlincoln.com>
2020-02-14 10:14:31 -0600

    more polish ui

--- app/src/polish/java/com/rthqks/synapse/polish/PolishActivity.kt
+++ app/src/polish/java/com/rthqks/synapse/polish/PolishActivity.kt
@@ -6,13 +6,22 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.edit
+import androidx.core.view.doOnLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.rthqks.synapse.R
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.polish.activity_polish.*
+
 
 class PolishActivity : DaggerAppCompatActivity() {
     private lateinit var preferences: SharedPreferences
@@ -53,10 +62,101 @@ class PolishActivity : DaggerAppCompatActivity() {
     private fun onReady() {
         val behavior = BottomSheetBehavior.from(layout_colors)
         behavior.state = BottomSheetBehavior.STATE_HIDDEN
+        var recording = false
 
-        layout_colors
+        surface_view.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceChanged(
+                holder: SurfaceHolder?,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                Log.d(TAG, "surfaceChanged $width $height")
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder?) {
+                Log.d(TAG, "surfaceDestroyed")
+            }
+
+            override fun surfaceCreated(holder: SurfaceHolder?) {
+                Log.d(TAG, "surfaceCreated")
+            }
+        })
+
         button_color.setOnClickListener {
+            Log.d(TAG, "show luts")
             behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        }
+
+        button_camera.setOnClickListener {
+            Log.d(TAG, "flip camera")
+        }
+
+        button_record.setOnClickListener {
+            recording = !recording
+            if (recording) {
+                focusMode()
+            } else {
+                exploreMode()
+            }
+            Log.d(TAG, "recording $recording")
+        }
+
+        button_settings.setOnClickListener {
+            Log.d(TAG, "show settings")
+
+        }
+
+        val snapHelper = LinearSnapHelper()
+
+        val layoutManager = effect_list.layoutManager as LinearLayoutManager
+        effect_list.adapter = EffectAdapter()
+
+        effect_list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                when (newState) {
+                    RecyclerView.SCROLL_STATE_IDLE -> {
+                        val view = snapHelper.findSnapView(layoutManager)
+                        val pos = view?.let { effect_list.getChildAdapterPosition(it) }
+                        Log.d(TAG, "pos $pos")
+
+                    }
+                }
+            }
+        })
+
+        effect_list.doOnLayout {
+            it.setPadding(it.width / 2, 0, it.width / 2, 0)
+            effect_list.scrollToPosition(0)
+            snapHelper.attachToRecyclerView(effect_list)
+        }
+    }
+
+    private fun focusMode() {
+        listOf(
+            button_camera,
+            button_settings,
+            button_color
+        ).forEach {
+            it.animate()
+                .alpha(0f)
+                .setDuration(200)
+                .withEndAction { it.visibility = View.GONE }
+                .start()
+        }
+    }
+
+    private fun exploreMode() {
+        listOf(
+            button_camera,
+            button_settings,
+            button_color
+        ).forEach {
+            it.visibility = View.VISIBLE
+            it.animate()
+                .alpha(1f)
+                .setDuration(200)
+                .start()
         }
     }
 
@@ -110,4 +210,34 @@ class PolishActivity : DaggerAppCompatActivity() {
     }
 }
 
-data class Permission(val name: String, val granted: Boolean, val showRationale: Boolean)
+private data class Permission(val name: String, val granted: Boolean, val showRationale: Boolean)
+
+private class EffectAdapter : RecyclerView.Adapter<EffectViewHolder>() {
+    private val effects = listOf(
+        "none",
+        "time warp",
+        "sparkle",
+        "trip",
+        "topography"
+    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EffectViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_effect, parent, false)
+        return EffectViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return effects.size
+    }
+
+    override fun onBindViewHolder(holder: EffectViewHolder, position: Int) {
+        when (position) {
+//            0 -> (holder.itemView as TextView).width = parentWidth / 2
+//            effects.size + 1 -> (holder.itemView as TextView).width = parentWidth / 2
+            else -> (holder.itemView as TextView).text = effects[position]
+        }
+
+    }
+}
+
+private class EffectViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
